5eff31c52cad5cd515eade4d9fc8bc9543d073e2
Lutz Rosema <terabaud@gmail.com>
2017-09-24 14:23:35 +0200

    cache the repo content in sessionStorage

--- src/index.js
+++ src/index.js
@@ -29,8 +29,13 @@ const http = (url, callback) => {
   xhr.send()
   xhr.onload = () => {
     const data = JSON.parse(xhr.responseText)
+    sessionStorage.setItem("repository_content", xhr.responseText)
     callback(data)
   }
 }
 
 http('https://api.github.com/repos/terabaud/css-practise/contents/src', render)
+if (! navigator.onLine) {
+  const offlineData = JSON.parse(sessionStorage.getItem("repository_content"))
+  render(offlineData)
+}
